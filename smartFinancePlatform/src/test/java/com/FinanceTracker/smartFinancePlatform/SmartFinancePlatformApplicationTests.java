7d6a5463482de542a7637ba52f1189c3ce670eff
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-02 11:12:52 +0200

    Prepare Backend Workplace

---
+++ smartFinancePlatform/src/test/java/com/FinanceTracker/smartFinancePlatform/SmartFinancePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinanceTracker.smartFinancePlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinancePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
