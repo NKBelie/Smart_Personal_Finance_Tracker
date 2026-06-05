2e2e72f557750d591f2aecd88d3eae59d2276943
NKBelie <belie.ndayisaba@auca.ac.rw>
2026-06-05 15:56:31 +0200

    Add model Package and Users class

---
+++ smartFinancePlatform/src/main/java/com/FinanceTracker/smartFinancePlatform/model/Users.java
@@ -0,0 +1,61 @@
+package com.FinanceTracker.smartFinancePlatform.model;
+
+public class Users {
+
+    private Long id;
+    private String fullName;
+    private String username;
+    private String email;
+    private String password;
+
+    // Constructors
+    public Users() {}
+
+    public Users(String fullName, String username, String email, String password) {
+        this.fullName = fullName;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
